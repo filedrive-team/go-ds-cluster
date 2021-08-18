fc259e6078d61fa5f297e68f4c3295cf41666aea
beeleelee <beeleelee@outlook.com>
2021-08-18 12:55:44 +0800

    store/store add read write funcs for messages

--- p2p/store/store.go
+++ p2p/store/store.go
@@ -29,6 +29,26 @@ func ReadRequestMsg(s network.Stream, msg *RequestMessage) error {
 	return nil
 }
 
+func WriteRequstMsg(s network.Stream, msg *RequestMessage) error {
+	if err := s.SetWriteDeadline(time.Now().Add(writeDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.WriteCborRPC(s, msg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadReplyMsg(s network.Stream, msg *ReplyMessage) error {
+	if err := s.SetReadDeadline(time.Now().Add(readDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.ReadCborRPC(s, msg); err != nil {
+		return err
+	}
+	return nil
+}
+
 func WriteReplyMsg(s network.Stream, msg *ReplyMessage) error {
 	if err := s.SetWriteDeadline(time.Now().Add(writeDeadline)); err != nil {
 		return err
