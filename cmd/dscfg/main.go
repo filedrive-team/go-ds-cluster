e455c2ab1b10b8aae85ce6826ddcc019155ae813
beeleelee <beeleelee@outlook.com>
2021-09-20 10:41:15 +0800

    feat(cmd):generate cluster side config file #8

--- cmd/dscfg/main.go
+++ cmd/dscfg/main.go
@@ -7,9 +7,11 @@ import (
 	"io/ioutil"
 	mrand "math/rand"
 	"os"
+	"path"
 	"time"
 
 	"github.com/filedrive-team/go-ds-cluster/config"
+	"github.com/filedrive-team/go-ds-cluster/shard"
 	log "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -26,6 +28,7 @@ func init() {
 func main() {
 	local := []*cli.Command{
 		clientCmd,
+		clusterCmd,
 	}
 
 	app := &cli.App{
@@ -37,6 +40,90 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
+}
+
+var clusterCmd = &cli.Command{
+	Name:  "cluster",
+	Usage: "generate cluster config files",
+	Flags: []cli.Flag{
+		&cli.IntFlag{
+			Name:  "cluster-node-number",
+			Value: 3,
+			Usage: "specify the cluster node number",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		outdir := c.Args().First()
+		if outdir == "" {
+			return xerrors.Errorf("usage: dscfg client [output-dir]")
+		}
+		finfo, err := os.Stat(outdir)
+		if err != nil {
+			return err
+		}
+		if !finfo.IsDir() {
+			return xerrors.Errorf("usage: dscfg client [output-dir]")
+		}
+		nodeNum := c.Int("cluster-node-number")
+		nodeIdentities := make([]config.Identity, nodeNum)
+		for i := range nodeIdentities {
+			priv, _, err := crypto.GenerateECDSAKeyPair(rand.Reader)
+			if err != nil {
+				return err
+			}
+			sk, err := priv.Bytes()
+			if err != nil {
+				return err
+			}
+
+			pid, err := peer.IDFromPrivateKey(priv)
+			if err != nil {
+				return err
+			}
+			nodeIdentities[i] = config.Identity{
+				PeerID: pid.Pretty(),
+				SK:     sk,
+			}
+		}
+		shardStartNodes := make([]shard.Node, nodeNum)
+		for i := range shardStartNodes {
+			shardStartNodes[i] = shard.Node{
+				ID: nodeIdentities[i].PeerID,
+			}
+		}
+		shardStartNodes = shard.InitSlotManager(shardStartNodes).Nodes()
+
+		cfgNodes := make([]config.Node, nodeNum)
+		for i := range cfgNodes {
+			rport := randPortNumber()
+			cfgNodes[i] = config.Node{
+				Node: shardStartNodes[i],
+				Swarm: []string{
+					fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", rport),
+					fmt.Sprintf("/ip4/0.0.0.0/udp/%s/quic", rport),
+				},
+			}
+		}
+
+		for i := range nodeIdentities {
+			clientCfg := &config.Config{
+				Addresses: config.Addresses{
+					Swarm: cfgNodes[i].Swarm,
+				},
+				Identity: nodeIdentities[i],
+				Nodes:    cfgNodes,
+			}
+			cfgbytes, err := json.MarshalIndent(&clientCfg, "", "\t")
+			if err != nil {
+				return err
+			}
+			err = ioutil.WriteFile(path.Join(outdir, fmt.Sprintf("cluster_%02d.json", i)), cfgbytes, 0644)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	},
 }
 
 var clientCmd = &cli.Command{
@@ -85,7 +172,7 @@ var clientCmd = &cli.Command{
 }
 
 func randPortNumber() string {
-	mrand.Seed(time.Now().Unix())
+	mrand.Seed(time.Now().Unix() * int64(mrand.Intn(9999)))
 	r := mrand.Float64()
 	m := 1000 + 9000*r
 	return fmt.Sprintf("%.0f", m)
