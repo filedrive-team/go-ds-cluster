f76283ff72a7b217273263d30b70349adfa2c35e
beeleelee <beeleelee@outlook.com>
2021-10-20 13:02:22 +0800

    feat:(p2p/share):gen cbor type

--- p2p/share/share.go
+++ p2p/share/share.go
@@ -1,9 +1,12 @@
+// share cluster nodes info
 package share
 
 import (
 	"time"
 
+	cborutil "github.com/filecoin-project/go-cbor-util"
 	log "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p-core/network"
 )
 
 var logging = log.Logger("dscluster/p2p/share")
@@ -14,3 +17,76 @@ const (
 
 var readDeadline = time.Second * 20
 var writeDeadline = time.Second * 20
+
+type InfoType uint8
+
+const (
+	InfoClusterNodes InfoType = 1 + iota
+)
+
+type ErrCode uint8
+
+const (
+	ErrNone ErrCode = iota
+	ErrNotFound
+
+	ErrOthers = 100
+)
+
+type ShareRequest struct {
+	Type InfoType
+}
+
+type ShareReply struct {
+	Code ErrCode
+	Type InfoType
+	Info []byte
+}
+
+func ReadRequest(s network.Stream, msg *ShareRequest) error {
+	if err := s.SetReadDeadline(time.Now().Add(readDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.ReadCborRPC(s, msg); err != nil {
+		_ = s.SetReadDeadline(time.Time{})
+		return err
+	}
+	_ = s.SetReadDeadline(time.Time{})
+	return nil
+}
+
+func WriteRequst(s network.Stream, msg *ShareRequest) error {
+	if err := s.SetWriteDeadline(time.Now().Add(writeDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.WriteCborRPC(s, msg); err != nil {
+		_ = s.SetWriteDeadline(time.Time{})
+		return err
+	}
+	_ = s.SetWriteDeadline(time.Time{})
+	return nil
+}
+
+func ReadReply(s network.Stream, msg *ShareReply) error {
+	if err := s.SetReadDeadline(time.Now().Add(readDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.ReadCborRPC(s, msg); err != nil {
+		_ = s.SetReadDeadline(time.Time{})
+		return err
+	}
+	_ = s.SetReadDeadline(time.Time{})
+	return nil
+}
+
+func WriteReply(s network.Stream, msg *ShareReply) error {
+	if err := s.SetWriteDeadline(time.Now().Add(writeDeadline)); err != nil {
+		return err
+	}
+	if err := cborutil.WriteCborRPC(s, msg); err != nil {
+		_ = s.SetWriteDeadline(time.Time{})
+		return err
+	}
+	_ = s.SetWriteDeadline(time.Time{})
+	return nil
+}
