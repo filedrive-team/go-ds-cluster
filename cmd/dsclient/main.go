ac14b2f00d5e6d67686389e0ab6c4e7cdce66471
beeleelee <beeleelee@outlook.com>
2021-10-20 18:52:19 +0800

    feat(cmd/dsclient):add init cmd get cluster nodes info from bootstrapper and write to config file #38

--- cmd/dsclient/main.go
+++ cmd/dsclient/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path"
 
@@ -11,6 +13,9 @@ import (
 	"github.com/filedrive-team/filehelper/dataset"
 	"github.com/filedrive-team/go-ds-cluster/clusterclient"
 	"github.com/filedrive-team/go-ds-cluster/config"
+	"github.com/filedrive-team/go-ds-cluster/p2p"
+	"github.com/filedrive-team/go-ds-cluster/p2p/share"
+	"github.com/filedrive-team/go-ds-cluster/utils"
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
@@ -22,7 +27,9 @@ import (
 	log "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
 	ufsio "github.com/ipfs/go-unixfs/io"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/mitchellh/go-homedir"
+	ma "github.com/multiformats/go-multiaddr"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
@@ -39,6 +46,7 @@ func main() {
 		importDatasetCmd,
 		statCmd,
 		getCmd,
+		initCmd,
 	}
 
 	app := &cli.App{
@@ -100,10 +108,21 @@ var addCmd = &cli.Command{
 	Name:  "add",
 	Usage: "import single file to ds-cluster",
 	Action: func(c *cli.Context) error {
-		cfg, err := config.ReadConfig(path.Join(c.String("conf"), config.DefaultConfigJson))
+		confPath := c.String("conf")
+		confPath, err := homedir.Expand(confPath)
 		if err != nil {
 			return err
 		}
+		err = os.MkdirAll(confPath, 0755)
+		if err != nil {
+			return err
+		}
+
+		cfg, err := config.ReadConfig(path.Join(confPath, config.DefaultConfigJson))
+		if err != nil {
+			return err
+		}
+
 		target := c.Args().First()
 		target, err = homedir.Expand(target)
 		if err != nil {
@@ -148,10 +167,21 @@ var statCmd = &cli.Command{
 	Name:  "stat",
 	Usage: "",
 	Action: func(c *cli.Context) error {
-		cfg, err := config.ReadConfig(path.Join(c.String("conf"), config.DefaultConfigJson))
+		confPath := c.String("conf")
+		confPath, err := homedir.Expand(confPath)
 		if err != nil {
 			return err
 		}
+		err = os.MkdirAll(confPath, 0755)
+		if err != nil {
+			return err
+		}
+
+		cfg, err := config.ReadConfig(path.Join(confPath, config.DefaultConfigJson))
+		if err != nil {
+			return err
+		}
+
 		target := c.Args().First()
 		tcid, err := cid.Decode(target)
 		if err != nil {
@@ -193,10 +223,21 @@ var getCmd = &cli.Command{
 	Name:  "get",
 	Usage: "",
 	Action: func(c *cli.Context) error {
-		cfg, err := config.ReadConfig(c.String("conf"))
+		confPath := c.String("conf")
+		confPath, err := homedir.Expand(confPath)
 		if err != nil {
 			return err
 		}
+		err = os.MkdirAll(confPath, 0755)
+		if err != nil {
+			return err
+		}
+
+		cfg, err := config.ReadConfig(path.Join(confPath, config.DefaultConfigJson))
+		if err != nil {
+			return err
+		}
+
 		args := c.Args().Slice()
 		tcid, err := cid.Decode(args[0])
 		if err != nil {
@@ -243,4 +284,90 @@ var getCmd = &cli.Command{
 
 		return nil
 	},
+}
+
+var initCmd = &cli.Command{
+	Name: "init",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "bootstrapper",
+			Usage: "",
+		},
+	},
+	Usage: "",
+	Action: func(c *cli.Context) error {
+		confPath := c.String("conf")
+		confPath, err := homedir.Expand(confPath)
+		if err != nil {
+			return err
+		}
+		err = os.MkdirAll(confPath, 0755)
+		if err != nil {
+			return err
+		}
+
+		cfg, err := config.ReadConfig(path.Join(confPath, config.DefaultConfigJson))
+		if err != nil {
+			cfg, err = config.GenClientConf()
+			if err != nil {
+				return err
+			}
+		}
+		ctxbg := context.Background()
+		bootstrapper := c.String("bootstrapper")
+		if len(cfg.Nodes) == 0 {
+			if bootstrapper == "" {
+				return xerrors.Errorf("missing cluster nodes config info")
+			}
+			err = initClientConfig(ctxbg, cfg, confPath, bootstrapper)
+			if err != nil {
+				logging.Error(err)
+				return err
+			}
+		}
+		bs, err := json.MarshalIndent(cfg, "", "\t")
+		if err != nil {
+			return err
+		}
+		logging.Infof("%s", bs)
+		return nil
+	},
+}
+
+func initClientConfig(ctxbg context.Context, cfg *config.Config, confPath, bootstrapper string) (err error) {
+	h1, err := p2p.MakeBasicHost(utils.RandPort())
+	if err != nil {
+		return
+	}
+	defer h1.Close()
+	baddr, err := ma.NewMultiaddr(bootstrapper)
+	if err != nil {
+		return
+	}
+	pinfo, err := peer.AddrInfoFromP2pAddr(baddr)
+	if err != nil {
+		return
+	}
+
+	client := share.NewShareClient(ctxbg, h1, *pinfo)
+
+	bs, err := client.GetClusterInfo()
+	if err != nil {
+		return
+	}
+	nodes := make([]config.Node, 0)
+	err = json.Unmarshal(bs, &nodes)
+	if err != nil {
+		return
+	}
+	cfg.Nodes = nodes
+	cfgbs, err := json.MarshalIndent(cfg, "", "\t")
+	if err != nil {
+		return
+	}
+	err = ioutil.WriteFile(path.Join(confPath, config.DefaultConfigJson), cfgbs, 0644)
+	if err != nil {
+		return
+	}
+	return
 }
