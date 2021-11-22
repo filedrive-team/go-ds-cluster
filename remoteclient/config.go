04a1876328f86d5e97ce65d88328e17657c3123f
beeleelee <beeleelee@outlook.com>
2021-11-22 17:00:19 +0800

    feat:remoteclient read config

--- remoteclient/config.go
+++ remoteclient/config.go
@@ -1,9 +1,27 @@
 package remoteclient
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Config struct {
-	AccessToken string
-	Bucket      string
-	Timeout     int
-	SwarmKey    string
-	Target      string
+	AccessToken string `json:"access_token"`
+	Bucket      string `json:"bucket"`
+	Timeout     int    `json:"timeout"`
+	SwarmKey    string `json:"swarmkey"`
+	Target      string `json:"target"`
+}
+
+func ReadConfig(path string) (*Config, error) {
+	cfg := new(Config)
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
 }
