f10a650faa889ab1a2035db0adab06a5d5476417
beeleelee <beeleelee@outlook.com>
2022-03-13 20:01:02 +0800

    updates

--- cmd/dscluster/main.go
+++ cmd/dscluster/main.go
@@ -239,6 +239,8 @@ func Kickoff(lc fx.Lifecycle, h host.Host, pid protocol.ID, ds ds.Datastore, cfg
 func BadgerDS(cfg *config.Config) (ds.Datastore, error) {
 	p := cfg.ConfPath + "/blocks"
 	opts := badgerds.DefaultOptions
+	// set value log file size: 16 GiB
+	opts.ValueLogFileSize = 16<<30 - 1
 	return badgerds.NewDatastore(p, &opts)
 }
 
